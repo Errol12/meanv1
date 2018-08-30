406a03a768fbdf317448666dbfc9263eb80f4ddf
Errol Fernandes <errol.fernandes@atos.net>
2018-08-30 17:13:53 +0530

    Fixed ngmodel two binding bug
    Signed-off-by: Errol Fernandes <errol.fernandes@atos.net>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,6 +18,7 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.css'],
   inputs:['video']
 })
+
+
 export class VideoDetailComponent implements OnInit {
 
   constructor() { }
